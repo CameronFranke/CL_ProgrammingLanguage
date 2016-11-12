a7eedde3823d6e80b3d9872a4b5f1a0d374cc607
cameron <C_franke1@u.pacific.edu>
2016-11-12 14:50:22 -0800

    added seperate semantics file, code gen init

---
+++ clparsersemantics.py
@@ -0,0 +1,61 @@
+class UnknownSemantics(object):
+    def alpha(self, ast):
+        return ast
+
+    def alphanumeric(self, ast):
+        return ast
+
+    def numeric(self, ast):
+        return ast
+
+    def word(self, ast):
+        return ast
+
+    def operator(self, ast):
+        d = {'type': "operator", 'value': ast}
+        return d
+
+    def var_name(self, ast):
+        return {'type': "var_name", 'value': ast}
+
+    def literal(self, ast):
+        return {"type": "literal", "value": ast}
+
+    def type_keyword(self, ast):
+        return {"type": "type_keyword", "value": ast}
+
+    def type_declaration(self, ast):
+        return {"type": "type_declaration", "value": ast}
+
+    def expression(self, ast):
+        return {"type": "expression", "value": ast}
+
+    def value(self, ast):
+        return {"type": "value", "value": ast}
+
+    def assignment(self, ast):
+        return {"type": "assignment", "value": ast}
+
+    def function_definition(self, ast):
+        return {"type": "function_definition", "value": ast}
+
+    def function_call(self, ast):
+        return {"type": "function_call", "value": ast}
+
+    def condition_statement(self, ast):
+        return {"type": "condition_statement", "value": ast}
+
+    def loop_statement(self, ast):
+        return {"type": "loop_statement", "value": ast}
+
+    def control_statement(self, ast):
+        return ast
+
+    def block(self, ast):
+        return {'type': "block", 'value': ast}
+
+    def program(self, ast):
+        return {"type": "program", "value": ast}
+
+    def comment(self, ast):
+        return ast
---
+++ codeGenerator.py
@@ -0,0 +1,59 @@
+
+import json
+import clparser
+import clparsersemantics 
+
+class codeGenerator(): 
+	def __init__(self, fileName):
+		self.sourceFile = fileName
+		self.sourceText = (open(self.sourceFile, "r").readlines())
+		self.printSourceText = True 
+		self.preprocessing()
+
+		self.parser = clparser.UnknownParser(parseinfo=True)
+		self.parseTree = self.parser.parse(self.sourceText, rule_name='program', semantics=clparsersemantics.UnknownSemantics())
+		self.printTree = True 
+		self.printParseTree()
+		
+
+	def preprocessing(self):
+                temp = []
+                skip = False
+                for i, line in enumerate(self.sourceText):
+                        if skip:
+                                skip = False
+                                continue
+                        if "&" in line:
+                                tempStr = line.replace("&", test[i+1].replace("\n", ""))
+                                skip = True
+                                temp.append(tempStr)
+                        else:
+				if line != "" and line != "\n":
+                                	temp.append(line)
+                
+			processedText = ""
+                for x in temp:
+                        processedText += x
+
+                processedText = "@@@" + processedText + "@@@"       # using "@@@" to denote start and end of file makes
+                							# some parseing tasks easier 
+		if self.printSourceText: 
+			print("\n================== " + self.sourceFile + " ==================")
+			print processedText + "\n"
+			
+		self.sourceText = str(processedText)
+
+	def printParseTree(self): 
+		if self.printTree: 
+			print("\n================== Syntax Tree  ==================")
+                	print(json.dumps(self.parseTree, indent=2)) # ASTs are JSON-friendy
+			print()
+
+
+
+
+
+
+
+
+x = codeGenerator("tests/cgt1")
